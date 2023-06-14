9c1da7b9168956bfcfb0e10f1709b199b62f6498
alexxandrrar <alexandroovnaa@gmail.com>
2023-06-14 20:18:28 +0300

    input component added

---
+++ client/src/components/Input/Input.tsx
@@ -0,0 +1,60 @@
+import { FC, useState } from 'react';
+import classNames from 'classnames';
+import {
+  EyeInvisibleOutlined,
+  EyeOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+import './Input.scss';
+
+interface IInputProps {
+  type: 'password' | 'email' | 'text';
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  placeholder?: string;
+  value: string;
+  required?: boolean;
+  error?: boolean;
+}
+
+export const Input: FC<IInputProps> = ({
+  type,
+  placeholder,
+  value,
+  required,
+  className,
+  error,
+  onChange,
+}): JSX.Element => {
+  const [inputType, setInputType] = useState(type);
+  return (
+    <div className={'input-container'}>
+      {type === 'password' &&
+        (inputType === 'password' ? (
+          <EyeInvisibleOutlined
+            className={'icon'}
+            onClick={() => setInputType('text')}
+          />
+        ) : (
+          <EyeOutlined
+            className={'icon'}
+            onClick={() => setInputType('password')}
+          />
+        ))}
+      {type === 'email' && (
+        <span className={'icon'}>
+          <MailOutlined />
+        </span>
+      )}
+      <input
+        type={type === 'password' ? inputType : type}
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        className={classNames(className, 'input')}
+        maxLength={type === 'password' ? 30 : 500}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
